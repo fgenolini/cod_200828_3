46376b341880db3c60eb865cd64414ddbd7c717d
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-30 00:33:43 +0100

    Update array_binary_search.h
    refactoring for clarity

--- array_binary_search.h
+++ array_binary_search.h
@@ -8,6 +8,42 @@
 
 namespace frank::algo {
 
+template <std::size_t SIZE>
+search_result left_search(std::span<int, SIZE> const sorted_array,
+                          int value_to_find, std::size_t middle) {
+  auto half_middle = middle / 2;
+  auto left_results = array_linear_search(
+      sorted_array, value_to_find, middle, search_option::EXACT,
+      search_direction::BACKWARD, half_middle);
+  if (left_results.has_result || left_results.value < value_to_find)
+    return left_results;
+
+  auto left_steps = left_results.search_steps;
+  auto right_results =
+      array_linear_search(sorted_array, value_to_find, 0, search_option::EXACT,
+                          search_direction::FORWARD, half_middle);
+  right_results.search_steps += left_steps;
+  return right_results;
+}
+
+template <std::size_t SIZE>
+search_result right_search(std::span<int, SIZE> const sorted_array,
+                           int value_to_find, std::size_t middle) {
+  auto half_middle = middle / 2;
+  auto right_results = array_linear_search(
+      sorted_array, value_to_find, middle, search_option::EXACT,
+      search_direction::FORWARD, half_middle);
+  if (right_results.has_result || right_results.value > value_to_find)
+    return right_results;
+
+  auto right_steps = right_results.search_steps;
+  auto left_results =
+      array_linear_search(sorted_array, value_to_find, 0, search_option::EXACT,
+                          search_direction::BACKWARD, half_middle);
+  left_results.search_steps += right_steps;
+  return left_results;
+}
+
 // Rudimentary improvement over linear search
 // by simple single "divide and conquer" / two step dichotomy
 template <std::size_t SIZE>
@@ -22,34 +58,10 @@ search_result array_binary_search(std::span<int, SIZE> const sorted_array,
 
   auto middle = sorted_array.size() / 2;
   auto middle_value = sorted_array[middle];
-  auto half_middle = middle / 2;
-  if (value_to_find <= middle_value) {
-    auto left_results = array_linear_search(
-        sorted_array, value_to_find, middle, search_option::EXACT,
-        search_direction::BACKWARD, half_middle);
-    if (left_results.has_result || left_results.value < value_to_find)
-      return left_results;
+  if (value_to_find <= middle_value)
+    return left_search(sorted_array, value_to_find, middle);
 
-    auto left_steps = left_results.search_steps;
-    auto right_results = array_linear_search(
-        sorted_array, value_to_find, 0, search_option::EXACT,
-        search_direction::FORWARD, half_middle);
-    right_results.search_steps += left_steps;
-    return right_results;
-  }
-
-  auto right_results = array_linear_search(
-      sorted_array, value_to_find, middle, search_option::EXACT,
-      search_direction::FORWARD, half_middle);
-  if (right_results.has_result || right_results.value > value_to_find)
-    return right_results;
-
-  auto right_steps = right_results.search_steps;
-  auto left_results =
-      array_linear_search(sorted_array, value_to_find, 0, search_option::EXACT,
-                          search_direction::BACKWARD, half_middle);
-  left_results.search_steps += right_steps;
-  return left_results;
+  return right_search(sorted_array, value_to_find, middle);
 }
 
 } // namespace frank::algo

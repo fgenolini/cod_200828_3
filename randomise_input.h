7323a742f7c6bc0626c6932f7f6a71454e85a40c
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-30 04:30:25 +0100

    improve output
    show timing of random input data generation, as well as array copy

--- randomise_input.h
+++ randomise_input.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <chrono>
 #include <cstddef>
 #include <iostream>
 #include <random>
@@ -25,11 +26,27 @@ void randomise_input(std::span<int, SIZE> random_data,
   auto memory_used_gb = memory_used / (1024.0 * 1024.0 * 1024.0);
   std::cerr << "Memory used: " << memory_used_gb << " GiB\n";
   std::cerr << "Generating random numbers...\n";
+  auto start_random = std::chrono::high_resolution_clock::now();
   std::generate(random_data.begin(), random_data.end(),
                 [&]() { return random_distribution(mersenne_twister_engine); });
+  auto stop_random = std::chrono::high_resolution_clock::now();
+  auto random_duration =
+      duration_cast<std::chrono::microseconds>(stop_random - start_random);
+  std::cerr << "  | random generation duration: " << random_duration.count()
+            << " us\n";
+
   std::cerr << "Copying...\n";
+  auto start_copy = std::chrono::high_resolution_clock::now();
+
+  // Slower than for loop:
+  // std::copy(random_data.begin(), random_data.end(), sorted_data.begin());
   for (std::size_t i = 0; i < random_data.size(); ++i)
     sorted_data[i] = random_data[i];
+
+  auto stop_copy = std::chrono::high_resolution_clock::now();
+  auto copy_duration =
+      duration_cast<std::chrono::microseconds>(stop_copy - start_copy);
+  std::cerr << "  | copy duration: " << copy_duration.count() << " us\n";
 }
 
 } // namespace frank::algo
--- timed_sort.h
+++ timed_sort.h
@@ -9,14 +9,14 @@ namespace frank::algo {
 
 template <std::size_t SIZE> void timed_sort(std::span<int, SIZE> &sorted_data) {
   std::cerr << "Sorting...\n";
-  auto sort_start = std::chrono::high_resolution_clock::now();
+  auto start_sort = std::chrono::high_resolution_clock::now();
 
   // C ::qsort is much slower (such as twice as slow)
   std::sort(sorted_data.begin(), sorted_data.end());
-  auto sort_stop = std::chrono::high_resolution_clock::now();
+  auto stop_sort = std::chrono::high_resolution_clock::now();
   auto sort_duration =
-      duration_cast<std::chrono::microseconds>(sort_stop - sort_start);
-  std::cerr << "Sort duration: " << sort_duration.count() << " us\n";
+      duration_cast<std::chrono::microseconds>(stop_sort - start_sort);
+  std::cerr << " | sort duration: " << sort_duration.count() << " us\n";
 }
 
 } // namespace frank::algo

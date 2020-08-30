91f832b38f40112e1bb76fe3c3ac2f759a21c118
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-30 15:17:49 +0100

    Update timed_sort.h
    tidy up

--- timed_sort.h
+++ timed_sort.h
@@ -24,17 +24,15 @@ void timed_sort(std::span<int, SIZE> sorted_data,
                 std::span<int, SIZE> qsorted_data) {
   std::cerr << "C++ STL sorting...\n";
   auto start_sort = std::chrono::high_resolution_clock::now();
-
-  // C ::qsort is much slower (such as twice as slow)
   std::sort(sorted_data.begin(), sorted_data.end());
   auto stop_sort = std::chrono::high_resolution_clock::now();
   auto sort_duration =
       duration_cast<std::chrono::microseconds>(stop_sort - start_sort);
   std::cerr << "  |  sort duration: " << sort_duration.count() << " us\n";
 
+  // C ::qsort is much slower (such as twice as slow) than C++ STL sort
   std::cerr << "C \"Quick\" sorting...\n";
   auto start_qsort = std::chrono::high_resolution_clock::now();
-
   ::qsort(qsorted_data.data(), qsorted_data.size(), sizeof(int), ascending);
   auto stop_qsort = std::chrono::high_resolution_clock::now();
   auto qsort_duration =

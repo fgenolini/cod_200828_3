18adf23ffd4908883499e839e40a829756fbf668
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-31 12:30:56 +0100

    added tick count
    Use the C ::clock function to get the tick count.
    Reformat output to debug console to line up information more clearly

--- timed_search.h
+++ timed_search.h
@@ -2,6 +2,8 @@
 
 #include <chrono>
 #include <cstddef>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <span>
 
@@ -19,19 +21,31 @@ template <std::size_t SIZE>
 search_result timed_search(std::span<int, SIZE> const sorted_data,
                            std::string message, int value_to_find,
                            search_func<SIZE> untimed_search) {
+  constexpr auto FIELD_WIDTH = 9;
+
   std::cerr << "Searching (" << message << ")...\n";
+  auto clock_start = ::clock();
   auto start = std::chrono::high_resolution_clock::now();
   auto results = untimed_search(sorted_data, value_to_find);
   auto stop = std::chrono::high_resolution_clock::now();
+  auto clock_stop = ::clock();
   auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-  std::cerr << "  | " << message << " search duration: " << duration.count()
-            << " us\n";
-  std::cerr << "  |    " << message << " search steps: " << results.search_steps
-            << '\n';
-  if (results.has_result)
-    std::cerr << "  |    " << message << " search value: " << results.value
-              << '\n';
+  auto clock_duration = clock_stop - clock_start;
+  std::cerr << "  | " << message
+            << " search duration: " << std::setw(FIELD_WIDTH)
+            << duration.count() << " us\n";
+  std::cerr << "  | " << message
+            << "    search clock: " << std::setw(FIELD_WIDTH) << clock_duration
+            << " ticks\n";
+  std::cerr << "  | " << message
+            << "    search steps: " << std::setw(FIELD_WIDTH)
+            << results.search_steps << '\n';
+  if (!results.has_result)
+    return results;
 
+  std::cerr << "  | " << message
+            << "    search value: " << std::setw(FIELD_WIDTH) << results.value
+            << '\n';
   return results;
 }
 
